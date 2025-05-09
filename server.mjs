8012e6336d3438a647467460e5859948209c4f5f
shadow1951 <roldannov@gmail.com>
2025-05-09 12:48:09 +0530

    host ready

--- server.mjs
+++ server.mjs
@@ -1,10 +1,24 @@
 import express from "express";
 import { middleware } from "./middleware.mjs";
 import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+
 dotenv.config();
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(helmet());
 app.use(express.json());
 app.post("/", middleware);
 
